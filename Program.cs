2a79a175e9ae0d7fbc24256128f2420b3783db77
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-14 19:06:24 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing is to prove , verify that our code is behaving cvorrectly !
+            // Testing  how the software behaves when thing go wrong, testing explores , investigates  how software behaves when things go wrong!
+            // how the software behaves when the user enteres illegal value , boundary values !
+            // unit is the sourceCode we want to test, unit is kind a  software code-module we want to test. 
+            // small unites of code we test, we test for example the methods !
+            // unit testing is automated.  We create,  write tests !
+            // we can run that test in the future with the help of , using a tool  Test Runner- >  tool to start the test /
+            // Test Runner tool -> finds all tests we have written, executes each test, and gives You a report , passed or anything failed !
+            // unit testing is software testing method by which individual usnits of source code, 
+            //one or more program modules together with associated control dataa, usage procedures and operating procedures are :
+            // tested to determine whether they are fit for use.
+            // Unit test are tipically automated tests written and run by software developers to ensure that  :
+            // a section of a application -known as unit meets its design  and behaves as intended. In Object Oriented programming, 
+            // unit can be interface, can  be class, can be method.
+            //the goal of the unitTesting, is to isolate each part of the program and to show that indivisual parts are correct.
+            // Aunit Test procides a strinct, written contract ahta the piece of the code must satisfy .
+            // Unit test finds problems int early phase on the development cycle !
+            // unit testing helpd to better structure the functions and opbject/
+            // unit testing help to refactor the code ! to chat the problems early!
+            // unit testing-test a given unit of the program, then the Integration testing test the whole bunch of the usnits.
+            ///if all parts work together !!
+            ///// By testing the parts of the program first and then testing the sum of teh parts , integration testin becaomes mus easier .
+            ///// unit tests provide a documentation of the whole system, \becasue developers look to learn the functionality of every unit,
+            ///and gow to use it //
+            ///to gain basic understanding of the whole system. //Unit testing provides a sort of living documentation of the system //
+            ///// unit tests checks the functionallity of the units. therefore it will not catch the integration errors or broader //
+            ///system -level errors
+            ///Tests only show  presence or absence of an error!
+            /////Integrastion testing heaviliy relie on human manual testing //High level or global scope testing can be  //
+            ///difficult to be automated !
+            ///
+            /* Unit testing is type of software testing where ndividual unists of components of as software are tested.the Purpose is to validate
+             * that each unit of the software code peforms as expected.unit testing  is donu during the devepolment coding phase //
+             * of an application by the developers.Unit test isolate a section   of the codeand verify its correctness /Aunit mau ne an individual
+             * funciton, mehtod, procedure, module, or object.
+             * Unit testing is the first level of tesing done by the developers, before the integrational testing .Unit testing is a whitebox, 
+             * testing technique htat is usially performed by  developer, in a pravtical world Qa engenieers also do unit testing !
+             
+             
+             
+             
+             */
+
+        }
+    }
+}
